9676b783c4e151e9e9c84693792eaf6507962728
David <davidianfoster@gmail.com>
2021-12-23 09:03:06 -0500

    OddOrEven complete

--- OddOrEven.py
+++ OddOrEven.py
@@ -1,10 +1,18 @@
+# https://www.practicepython.org/exercise/2014/02/05/02-odd-or-even.html
 
-num = input("Enter a number: ")
 
-if int(num) == 4:
-    print("You chose 4!")
-elif int(num) % 2 == 0:
-    print(num + " is even!")
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+
+if int(num1) % int(num2) == 0:
+    print(num1 + " is divisible by " + num2)
 else:
-    print(num + " is odd!")
+    print("The remained of " + num1 + " divided by " +
+          num2 + " is " + str(int(num1) % int(num2)) + ".")
 
+# if int(num) == 4:
+#     print("You chose 4!")
+# elif int(num) % 2 == 0:
+#     print(num + " is even!")
+# else:
+#     print(num + " is odd!")

3651b7616aa76fa766cb42be91187e89ecae886f
David <davidianfoster@gmail.com>
2021-12-25 17:38:17 -0500

    ListComprehensions complete

---
+++ ListComprehensions.py
@@ -0,0 +1,7 @@
+# https://www.practicepython.org/exercise/2014/03/19/07-list-comprehensions.html
+
+a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+evens = [x for x in a if x % 2 == 0]
+
+print(evens)
